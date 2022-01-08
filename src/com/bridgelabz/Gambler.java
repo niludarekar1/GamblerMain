75fb99b633a2db5731ae5876b5b8ae6a04e416c7
niludarekar1 <nilu.darekar1@gmail.com>
2022-01-08 16:21:33 +0530

    ADDED : Gambler Total Stake 100 Doller Every Day and Bet One Doller

---
+++ src/com/bridgelabz/Gambler.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class Gambler {
+
+       private static final int DAILY_STAKE=100;
+       private static final int GAME_BET=1;
+
+      public static void main(String[] args) {
+
+
+      }
+}
